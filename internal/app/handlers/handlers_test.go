c80dbd1690734585972cebba152de17aa1467cf9
Stepan Milyaev <milyaevs@quantumart.ru>
2021-11-20 19:07:00 +0300

    first unit tests (only shorten url method covered now)

---
+++ internal/app/handlers/handlers_test.go
@@ -0,0 +1,124 @@
+package handlers
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+type UrlStorageMock struct {
+	mock.Mock
+}
+
+func (m *UrlStorageMock) Store(url string) (string, error) {
+	args := m.Called(url)
+	return args.String(0), args.Error(1)
+}
+
+func (m *UrlStorageMock) Load(key string) (string, bool, error) {
+	args := m.Called(key)
+	return args.String(0), args.Bool(1), args.Error(2)
+}
+
+type HandlersTestSuite struct {
+	suite.Suite
+}
+
+func (suite *HandlersTestSuite) TestShortenHandler() {
+	type request struct {
+		url    string
+		method string
+		body   string
+	}
+	type want struct {
+		contentType string
+		statusCode  int
+		body        string
+	}
+	tests := []struct {
+		name    string
+		request request
+		want    want
+	}{
+		{
+			name: "should shorten google",
+			request: request{
+				url:    "/",
+				method: http.MethodPost,
+				body:   "http://google.com",
+			},
+			want: want{
+				contentType: "text/plain; charset=utf-8",
+				statusCode:  http.StatusCreated,
+				body:        "http://localhost:8080/shortGoogle",
+			},
+		},
+		{
+			name: "should fail on empty body",
+			request: request{
+				url:    "/",
+				method: http.MethodPost,
+				body:   "",
+			},
+			want: want{
+				statusCode: http.StatusBadRequest,
+			},
+		},
+		{
+			name: "should fail on relative url",
+			request: request{
+				url:    "/",
+				method: http.MethodPost,
+				body:   "/somerelativeurl",
+			},
+			want: want{
+				statusCode: http.StatusBadRequest,
+			},
+		},
+		{
+			name: "should fail on invalid url",
+			request: request{
+				url:    "/",
+				method: http.MethodPost,
+				body:   "some text",
+			},
+			want: want{
+				statusCode: http.StatusBadRequest,
+			},
+		},
+	}
+	urlStorage := new(UrlStorageMock)
+	urlStorage.On("Store", "http://google.com").Return("shortGoogle", nil).Once()
+	for _, tt := range tests {
+		suite.T().Run(tt.name, func(t *testing.T) {
+			request := httptest.NewRequest(tt.request.method, tt.request.url, strings.NewReader(tt.request.body))
+			w := httptest.NewRecorder()
+			h := shortenURLHandler(urlStorage)
+
+			h.ServeHTTP(w, request)
+
+			res := w.Result()
+
+			assert.Equal(t, tt.want.statusCode, res.StatusCode)
+			if res.StatusCode == http.StatusCreated {
+				assert.Equal(t, tt.want.contentType, res.Header.Get("Content-Type"))
+				defer res.Body.Close()
+				resBody, err := io.ReadAll(res.Body)
+				require.NoError(t, err)
+				assert.Equal(t, tt.want.body, string(resBody))
+			}
+		})
+	}
+
+	urlStorage.AssertExpectations(suite.T())
+}
+
+func TestHandlersTestSuite(t *testing.T) {
+	suite.Run(t, new(HandlersTestSuite))
+}
