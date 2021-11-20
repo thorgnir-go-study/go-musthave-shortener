a80dcb1facecf646a5346ec94e0f60c3b3e64f95
Stepan Milyaev <milyaevs@quantumart.ru>
2021-11-21 00:54:02 +0300

    fix

--- internal/app/handlers/router.go
+++ internal/app/handlers/router.go
@@ -2,11 +2,18 @@ package handlers
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/thorgnir-go-study/go-musthave-shortener/internal/app/storage"
+	"time"
 )
 
 func NewRouter(storage storage.URLStorage) chi.Router {
 	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Timeout(10 * time.Second))
 
 	r.Post("/", ShortenURLHandler(storage))
 	r.Get("/{urlID}", ExpandURLHandler(storage))
--- internal/app/server.go
+++ internal/app/server.go
@@ -2,20 +2,14 @@ package app
 
 import (
 	"fmt"
-	"github.com/go-chi/chi/v5/middleware"
 	"github.com/thorgnir-go-study/go-musthave-shortener/internal/app/handlers"
 	"github.com/thorgnir-go-study/go-musthave-shortener/internal/app/storage"
 	"net/http"
-	"time"
 )
 
 func StartURLShortenerServer(port uint16, storage storage.URLStorage) {
 	r := handlers.NewRouter(storage)
-	r.Use(middleware.RequestID)
-	r.Use(middleware.RealIP)
-	r.Use(middleware.Logger)
-	r.Use(middleware.Recoverer)
-	r.Use(middleware.Timeout(10 * time.Second))
+
 	err := http.ListenAndServe(fmt.Sprintf(":%d", port), r)
 	if err != nil {
 		panic(err)
