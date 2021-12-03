decc4bb389cfc2480ea1d7b6d6bbbbf30ba777c3
thorgnir <thorgnir@yandex.ru>
2021-12-03 20:42:02 +0300

    added CLI

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"github.com/caarlos0/env/v6"
 	"github.com/thorgnir-go-study/go-musthave-shortener/internal/app"
 	"github.com/thorgnir-go-study/go-musthave-shortener/internal/app/config"
@@ -8,15 +9,48 @@ import (
 	"log"
 )
 
+var (
+	serverAddressFlag *string
+	baseURLFlag       *string
+	storagePathFlag   *string
+)
+
+func init() {
+	serverAddressFlag = flag.String("a", "", "Server address. If not set in CLI or env variable SERVER_ADDRESS defaults to ':8080'")
+	baseURLFlag = flag.String("b", "", "Base URL. If not set in CLI or env variable BASE_URL defaults to http://localhost:8080")
+	storagePathFlag = flag.String("f", "", "File storage path. If not set in CLI or env variable FILE_STORAGE_PATH storage will be non-persistent")
+}
+
 func main() {
 	var cfg config.Config
-
 	err := env.Parse(&cfg)
 	if err != nil {
 		log.Fatalln("Error parsing config")
 	}
 
-	urlStorage, err := storage.CreateMapURLStorage(storage.WithFilePersistance(cfg.StorageFilePath))
+	flag.Parse()
+	if *serverAddressFlag != "" {
+		cfg.ServerAddress = *serverAddressFlag
+	}
+
+	if *baseURLFlag != "" {
+		cfg.BaseURL = *baseURLFlag
+	}
+
+	if *storagePathFlag != "" {
+		cfg.StorageFilePath = *storagePathFlag
+	}
+
+	urlStorage, err := storage.CreateMapURLStorage()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if cfg.StorageFilePath != "" {
+		err := storage.WithFilePersistance(cfg.StorageFilePath)(urlStorage)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
 	if err != nil {
 		log.Fatalln(err)
 	}
--- internal/app/config/config.go
+++ internal/app/config/config.go
@@ -3,5 +3,5 @@ package config
 type Config struct {
 	ServerAddress   string `env:"SERVER_ADDRESS" envDefault:":8080"`
 	BaseURL         string `env:"BASE_URL" envDefault:"http://localhost:8080"`
-	StorageFilePath string `env:"FILE_STORAGE_PATH" envDefault:"url_storage.txt"`
+	StorageFilePath string `env:"FILE_STORAGE_PATH"`
 }
