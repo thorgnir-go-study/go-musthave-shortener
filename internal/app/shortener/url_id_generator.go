d97ebf6a7be528ea90dc180a3349e77de61c2f58
thorgnir <thorgnir@yandex.ru>
2022-01-04 19:30:12 +0300

    moved random to package

--- internal/app/shortener/url_id_generator.go
+++ internal/app/shortener/url_id_generator.go
@@ -1,6 +1,6 @@
 package shortener
 
-import "math/rand"
+import "github.com/thorgnir-go-study/go-musthave-shortener/internal/app/random"
 
 type URLIDGenerator interface {
 	// GenerateURLID генерирует идентификатор сокращенной ссылки
@@ -16,16 +16,5 @@ func NewRandomStringURLIDGenerator(length int) *RandomStringURLIDGenerator {
 }
 
 func (g *RandomStringURLIDGenerator) GenerateURLID(originalURL string) string {
-	return randomString(g.length)
-}
-
-var letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-
-func randomString(n int) string {
-	// хороший пост про возможные оптимизации производительности генерации строки тут: https://stackoverflow.com/questions/22892120/how-to-generate-a-random-string-of-a-fixed-length-in-go
-	b := make([]byte, n)
-	for i := range b {
-		b[i] = letters[rand.Int63()%int64(len(letters))]
-	}
-	return string(b)
+	return random.String(g.length)
 }
